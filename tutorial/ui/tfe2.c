ea10e741bb9792efbe5ddc2ec54db35761c3b4c0
fetch150zy <Mars_zhewei@outlook.com>
2022-09-01 17:29:38 +0800

    Using XML to build UI

--- tutorial/ui/tfe2.c
+++ tutorial/ui/tfe2.c
@@ -1,79 +1,88 @@
 #include <gtk/gtk.h>
 
+
 #define TFE_TYPE_TEXT_VIEW tfe_text_view_get_type()
 G_DECLARE_FINAL_TYPE(TfeTextView, tfe_text_view, TFE, TEXT_VIEW, GtkTextView)
 
-struct _TfeTextView
-{
+
+struct _TfeTextView {
   GtkTextView parent;
   GFile *file;
 };
 
+
 G_DEFINE_TYPE(TfeTextView, tfe_text_view, GTK_TYPE_TEXT_VIEW);
+
 
 static void tfe_text_view_init(TfeTextView *tv)
 {
 }
 
+
 static void tfe_text_view_class_init(TfeTextViewClass *class)
 {
 }
 
+
 void tfe_text_view_set_file(TfeTextView *tv, GFile *f)
 {
-  tv->file = f;
+    tv->file = f;
 }
+
 
 GFile *tfe_text_view_get_file(TfeTextView *tv)
 {
-  return tv->file;
+    return tv->file;
 }
+
 
 GtkWidget *tfe_text_view_new(void)
 {
-  return GTK_WIDGET(g_object_new(TFE_TYPE_TEXT_VIEW, NULL));
+    return GTK_WIDGET(g_object_new(TFE_TYPE_TEXT_VIEW, NULL));
 }
+
 
 /* ---------- end of the definition of TfeTextView ---------- */
-
 static gboolean before_close(GtkWindow *win, gpointer user_data)
-{ 
-  GtkWidget *nb = GTK_WIDGET(user_data);
-  GtkWidget *scr;
-  GtkWidget *tv;
-  GFile *file;
-  char *pathname;
-  GtkTextBuffer *tb;
-  GtkTextIter start_iter;
-  GtkTextIter end_iter;
-  char *contents;
-  unsigned int n;
-  unsigned int i;
+{
+    GtkWidget *nb = GTK_WIDGET(user_data);
+    GtkWidget *scr;
+    GtkWidget *tv;
+    GFile *file;
+    char *pathname;
+    GtkTextBuffer *tb;
+    GtkTextIter start_iter;
+    GtkTextIter end_iter;
+    char *contents;
+    unsigned int n;
+    unsigned int i;
 
-  n = gtk_notebook_get_n_pages(GTK_NOTEBOOK(nb));
-  for (i = 0; i < n; ++i)
-  {
-    scr = gtk_notebook_get_nth_page(GTK_NOTEBOOK(nb), i);
-    tv = gtk_scrolled_window_get_child(GTK_SCROLLED_WINDOW(scr));
-    file = tfe_text_view_get_file(TFE_TEXT_VIEW(tv));
-    tb = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tv));
-    gtk_text_buffer_get_bounds(tb, &start_iter, &end_iter);
-    contents = gtk_text_buffer_get_text(tb, &start_iter, &end_iter, FALSE);
-    if (!g_file_replace_contents(file, contents, strlen(contents), NULL, TRUE, G_FILE_CREATE_NONE, NULL, NULL, NULL))
-    {
-      pathname = g_file_get_path(file);
-      g_print("ERROR : Can't save %s.", pathname);
-      g_free(pathname);
+    n = gtk_notebook_get_n_pages(GTK_NOTEBOOK(nb));
+    for (i = 0; i < n; ++i) {
+        scr = gtk_notebook_get_nth_page(GTK_NOTEBOOK(nb), i);
+        tv = gtk_scrolled_window_get_child(GTK_SCROLLED_WINDOW(scr));
+        file = tfe_text_view_get_file(TFE_TEXT_VIEW(tv));
+        tb = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tv));
+        gtk_text_buffer_get_bounds(tb, &start_iter, &end_iter);
+        contents = gtk_text_buffer_get_text(tb, &start_iter, &end_iter, FALSE);
+
+        if (!g_file_replace_contents(file, contents, strlen(contents), NULL,TRUE,
+                                     G_FILE_CREATE_NONE, NULL, NULL, NULL)) {
+            pathname = g_file_get_path(file);
+            g_print("ERROR : Can't save %s.", pathname);
+            g_free(pathname);
+        }
+        g_free(contents);
     }
-    g_free(contents);
-  }
-  return FALSE;
+    return FALSE;
 }
+
 
 static void app_activate(GApplication *app, gpointer user_data)
 { // 命令行调用时没有传入文件会调用这个函数
   g_print("You need to give filenames as arguments.\n");
 }
+
 
 // static void
 // app_open (GApplication *app, GFile ** files, gint n_files, gchar *hint, gpointer user_data) {
@@ -162,71 +171,70 @@ static void app_activate(GApplication *app, gpointer user_data)
 //     gtk_window_destroy (GTK_WINDOW (win));
 // }
 
-static void app_open(GApplication *app, GFile **files, gint n_files, gchar *hint, gpointer user_data)
+
+static void app_open(GApplication *app, GFile **files, 
+                     gint n_files, gchar *hint, gpointer user_data)
 {
-  GtkWidget *win;       // window
-  GtkWidget *nb;        // note book
-  GtkWidget *lab;       // label text
-  GtkNotebookPage *nbp; // note book page
-  GtkWidget *scr;       // scroll
-  GtkWidget *tv;        // text view
-  GtkTextBuffer *tb;    // text buffer
-  char *contents;       // text string
-  gsize length;         // text string length
-  char *filename;       // text file name
-  GtkBuilder *build;    // for build ui
+    GtkWidget *win;       // window
+    GtkWidget *nb;        // note book
+    GtkWidget *lab;       // label text
+    GtkNotebookPage *nbp; // note book page
+    GtkWidget *scr;       // scroll
+    GtkWidget *tv;        // text view
+    GtkTextBuffer *tb;    // text buffer
+    char *contents;       // text string
+    gsize length;         // text string length
+    char *filename;       // text file name
+    GtkBuilder *build;    // for build ui
 
-  // 实现一些功能键
-  build = gtk_builder_new_from_file("button.xml");
-  win = GTK_WIDGET(gtk_builder_get_object(build, "win"));
-  gtk_window_set_application(GTK_WINDOW(win), GTK_APPLICATION(app));
-  nb = GTK_WIDGET(gtk_builder_get_object(build, "nb")); // ui是渲染在note book上
-  g_object_unref(build);                                // 销毁build
-  for (int i = 0; i < n_files; i++)                     // 多文件显示
-  {
-    if (g_file_load_contents(files[i], NULL, &contents, &length, NULL, NULL))
-    {
-      scr = gtk_scrolled_window_new();
-      tv = tfe_text_view_new(); // text view是使用我们自己实现的子对象(gtk_text_view)
-      tb = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tv));
-      gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(tv), GTK_WRAP_WORD_CHAR);
-      gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scr), tv); // 滚动栏
+    // 实现一些功能键
+    build = gtk_builder_new_from_file("button.xml");
+    win = GTK_WIDGET(gtk_builder_get_object(build, "win"));
+    gtk_window_set_application(GTK_WINDOW(win), GTK_APPLICATION(app));
+    nb = GTK_WIDGET(gtk_builder_get_object(build, "nb")); // ui是渲染在note book上
+    g_object_unref(build);                                // 销毁build
 
-      tfe_text_view_set_file(TFE_TEXT_VIEW(tv), g_file_dup(files[i]));
-      gtk_text_buffer_set_text(tb, contents, length); // 把文本载入到text buffer中
-      g_free(contents);
-      filename = g_file_get_basename(files[i]);             // get filename
-      lab = gtk_label_new(filename);                        // 创建一个label,名为filename
-      gtk_notebook_append_page(GTK_NOTEBOOK(nb), scr, lab); // 把 scroll 和 label 加载到 note book 上
-      nbp = gtk_notebook_get_page(GTK_NOTEBOOK(nb), scr);
-      g_object_set(nbp, "tab-expand", TRUE, NULL); // 把 note book 组织到 note book page 上
-      g_free(filename);
+    for (int i = 0; i < n_files; i++) {
+        if (g_file_load_contents(files[i], NULL, &contents, &length, NULL, NULL)) {
+            scr = gtk_scrolled_window_new();
+            tv = tfe_text_view_new(); // text view是使用我们自己实现的子对象(gtk_text_view)
+            tb = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tv));
+            gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(tv), GTK_WRAP_WORD_CHAR);
+            gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scr), tv); // 滚动栏
+
+            tfe_text_view_set_file(TFE_TEXT_VIEW(tv), g_file_dup(files[i]));
+            gtk_text_buffer_set_text(tb, contents, length); // 把文本载入到text buffer中
+            g_free(contents);
+            filename = g_file_get_basename(files[i]);             // get filename
+            lab = gtk_label_new(filename);                        // 创建一个label,名为filename
+            gtk_notebook_append_page(GTK_NOTEBOOK(nb), scr, lab); // 把 scroll 和 label 加载到 note book 上
+            nbp = gtk_notebook_get_page(GTK_NOTEBOOK(nb), scr);
+            g_object_set(nbp, "tab-expand", TRUE, NULL); // 把 note book 组织到 note book page 上
+            g_free(filename);
+        }
+        else if ((filename = g_file_get_path(files[i])) != NULL) {
+            g_print("No such file: %s.\n", filename);
+            g_free(filename);
+        }
+        else
+            g_print("No valid file is given\n");
     }
-    else if ((filename = g_file_get_path(files[i])) != NULL)
-    {
-      g_print("No such file: %s.\n", filename);
-      g_free(filename);
-    }
+    if (gtk_notebook_get_n_pages(GTK_NOTEBOOK(nb)) > 0)
+        gtk_widget_show(win);
     else
-      g_print("No valid file is given\n");
-  }
-  if (gtk_notebook_get_n_pages(GTK_NOTEBOOK(nb)) > 0)
-  {
-    gtk_widget_show(win);
-  }
-  else
-    gtk_window_destroy(GTK_WINDOW(win));
+        gtk_window_destroy(GTK_WINDOW(win));
 }
+
 
 int main(int argc, char **argv)
 {
-  GtkApplication *app;
-  int stat;
+    GtkApplication *app;
+    int stat;
 
-  app = gtk_application_new("mars.demo", G_APPLICATION_HANDLES_OPEN);
-  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
-  g_signal_connect(app, "open", G_CALLBACK(app_open), NULL);
-  stat = g_application_run(G_APPLICATION(app), argc, argv);
-  g_object_unref(app);
-  return stat;
+    app = gtk_application_new("mars.demo", G_APPLICATION_HANDLES_OPEN);
+    g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+    g_signal_connect(app, "open", G_CALLBACK(app_open), NULL);
+    stat = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+    return stat;
 }
